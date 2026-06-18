585b183722285ab3eb43889a3d4739c2efcd2ee0
cosalamone <cosalamone@gmail.com>
2026-06-18 17:49:02 -0300

    configs para vercel

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://produccion.com',
+  apiUrl: '',
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useStaticAssets(join(__dirname, '..', 'uploads'), { prefix: '/uploads' });
-  app.enableCors();
+  app.enableCors({ origin: '*' });
   app.useBodyParser('json', { limit: '10mb' });
   app.useBodyParser('urlencoded', { extended: true, limit: '10mb' });
   await app.listen(process.env.PORT ?? 3000);
